a6ba7750aab650e427255d38feb84e2c0333a8d0
IAmGreyBunny <IAmGreyBunny@gmail.com>
2025-06-19 13:28:24 +0800

    Registration works in local server, need to work on sanity check, and refactor code to work with public server possibly through a config script

--- MampDocumentRoot/register.php
+++ MampDocumentRoot/register.php
@@ -7,22 +7,22 @@
 	if (mysqli_connect_errno())
 	{
 		echo "1"; //error code #1 = Connection failed
-		exit()
+		exit();
 	}
 
 	//Parse variables from caller's POST request
 	$username = $_POST['username'];
-	$password = $_POST['password'];
 	$email = $_POST['email'];
-
+	$password = $_POST['password'];
+	
 	//Check if username exists
 	$usernameCheckQuery = "SELECT username FROM players WHERE username='" . $username . "';";
 	$usernameQueryResult = mysqli_query($con,$usernameCheckQuery) or die("2: Username check query failed");
 
 	if(mysqli_num_rows($usernameQueryResult)>0)
 	{
-		echo "3: Name already exists"
-		exit()
+		echo "3: Name already exists";
+		exit();
 	}
 
 	//Check if email exists
@@ -31,20 +31,21 @@
 
 	if(mysqli_num_rows($emailQueryResult)>0)
 	{
-		echo "3: Email already exists"
-		exit()
+		echo "3: Email already exists";
+		exit();
 	}
 
 	//Generating password hash to keep password stored secured
-	$salt = "\$5\$rounds=5000\$" . "steamedhams" . $username . "\$" //Generate unique salt based on username and sha-256
+	$salt = "\$5\$rounds=5000\$" . "steamedhams" . $username . "\$"; //Generate unique salt based on username and sha-256
 	$hash = crypt($password,$salt); //encrypt hash based on salt generated
 
 	//Insert user into table
-	$insertUserQuery = "INSERT INTO players (username,hash,salt) VALUES (
+	$insertUserQuery = "INSERT INTO players (username,email,hash,salt) VALUES (
 		'" . $username . "',
+		'" . $email . "',
 		'" . $hash . "',
 		'" . $salt . "');";
 	mysqli_query($con,$insertUserQuery) or die("4: Insert player query failed");
 
-
+	echo "0";
 ?>
