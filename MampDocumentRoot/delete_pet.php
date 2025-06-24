de0dd652d9699607f51068e0b76554ddd59f33f5
IAmGreyBunny <IAmGreyBunny@gmail.com>
2025-06-24 23:16:48 +0800

    Added delete_pet.php, need further testing

---
+++ MampDocumentRoot/delete_pet.php
@@ -0,0 +1,50 @@
+<?php
+header('Content-Type: application/json');
+
+// Error info
+$status_code = 0;
+$error_message = "";
+
+//Connects to sql database
+require 'db_connect.php';
+
+//Parse variables from caller's POST request
+$json = json_decode(file_get_contents('php://input'),true);
+if($json === null){
+	echo json_encode([
+		"status_code" => 7,
+		"error_message" => "Invalid JSON"
+	]);
+	exit();
+}
+
+$pet_id = $json['pet_id'] ?? '';
+//Construct query depending on whether owner_id is specified, if
+if($owner_id)
+{
+	$dropPetQuery = "DROP * FROM pets WHERE pet_id='" . $pet_id . "';";
+}
+else
+{
+	echo json_encode([
+		"status_code" => 17,
+		"error_message" => "pet_id value not provided"
+	]);
+	exit();
+}
+$dropPetQueryResult = mysqli_query($con,$dropPetQuery);
+
+if (!$dropPetQuery) {
+	echo json_encode([
+		"status_code" => 18,
+		"error_message" => "Drop Pet Query Failed"
+	]);
+	exit();
+}
+
+echo json_encode([
+		"status_code" => $status_code,
+		"error_message" => $error_message
+	]);
+
+?>
