2e535874ec1310c98496ccd7ef650985c29f68e9
JayHaych <teejiahong1207@gmail.com>
2025-06-24 15:32:32 +0800

    shop script

---
+++ FinaPet/Assets/Scripts/ShopController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShopController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetShopItems();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator GetShopItems()
+    {
+
+
+        string apiPath = ServerConfig.LoadFromFile("Config/ServerConfig.json").GetApiPath();
+        Debug.Log("API Path: " + apiPath);
+        UnityWebRequest request = UnityWebRequest.PostWwwForm(apiPath + "/get_shop_items.php", "");
+        yield return request.SendWebRequest();
+
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Request error: " + request.error);
+        }
+        else
+        {
+            _GetShopItemsResponse getShopItemsResponse = JsonUtility.FromJson<_GetShopItemsResponse>(request.downloadHandler.text);
+
+            if (getShopItemsResponse.status_code == 0)
+            {
+                Debug.Log(getShopItemsResponse.shop_items.Length);
+            }
+            else
+            {
+                Debug.Log("Get shop item Failed with message: " + getShopItemsResponse.error_message);
+            }
+        }
+    }
+
+    [Serializable]
+    private class _GetShopItemsResponse
+    {
+        public int status_code;
+        public string error_message;
+        public _ShopItem[] shop_items;
+    }
+
+    [Serializable]
+    private class _ShopItem
+    {
+        public int item_id;
+        public string item_name;
+    }
+}
