25b8f0ae9f77bbee38851dd54fd8aedfe8b9ad81
IAmGreyBunny <IAmGreyBunny@gmail.com>
2025-06-25 21:13:01 +0800

    Moved ShopItem object into it's own script, fixed scrollview content so that we can use clamped for scroll

--- FinaPet/Assets/Scripts/ShopController.cs
+++ FinaPet/Assets/Scripts/ShopController.cs
@@ -140,7 +140,7 @@ public class ShopController : MonoBehaviour
                 {
                     Debug.Log($"[ShopController] Number of items fetched: {getShopItemsResponse.items.Length}");
 
-                    foreach (_ShopItem item in getShopItemsResponse.items) //
+                    foreach (ShopItem item in getShopItemsResponse.items) //
                     {
                         GameObject currentShopCard = Instantiate(shopItemCardPrefab, shopItemParent);
                         // Find the TextMeshPro component for the item name and price
@@ -206,16 +206,6 @@ public class ShopController : MonoBehaviour
     {
         public int status_code; //
         public string error_message; //
-        public _ShopItem[] items; //
-    }
-
-    [Serializable]
-    private class _ShopItem
-    {
-        public int item_id; //
-        public string item_name; //
-        public int item_price; //
-        public string item_type;
-        // Add other properties from your PHP response if needed (e.g., description, image_url etc.)
+        public ShopItem[] items; //
     }
 }
---
+++ FinaPet/Assets/Scripts/ShopItem.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class ShopItem
+{
+    public int item_id; //
+    public string item_name; //
+    public int item_price; //
+    public string item_type;
+    // Add other properties from your PHP response if needed (e.g., description, image_url etc.)
+}
