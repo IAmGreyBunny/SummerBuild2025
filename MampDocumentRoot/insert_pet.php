509d38557318ee436de2c9e542fe1d5c67645b73
IAmGreyBunny <IAmGreyBunny@gmail.com>
2025-06-25 15:49:11 +0800

    insert_pet.php is done, need further testing

---
+++ MampDocumentRoot/insert_pet.php
@@ -0,0 +1,55 @@
+<?php
+header('Content-Type: application/json');
+
+// Error info
+$status_code = 0;
+$error_message = "";
+
+// Connect to SQL database
+require 'db_connect.php';
+
+// Parse JSON input
+$json = json_decode(file_get_contents('php://input'), true);
+if ($json === null) {
+	echo json_encode([
+		"status_code" => 7,
+		"error_message" => "Invalid JSON"
+	]);
+	exit();
+}
+
+$owner_id = $json['owner_id'] ?? '';
+if (!$pet_id) {
+	echo json_encode([
+		"status_code" => 10,
+		"error_message" => "player_id not provided"
+	]);
+	exit();
+}
+
+$affection = $json['affection'] ?? '';
+if (!$affection) {
+	echo json_encode([
+		"status_code" => 22,
+		"error_message" => "affection value not provided"
+	]);
+	exit();
+}
+
+// Query for player_data
+$updatePetAffectionQuery = "UPDATE pets SET affection = '" . $affection .  "' WHERE pet_id = '" . $pet_id . "';";
+$updatePetAffectionQueryResult = mysqli_query($con, $updatePetAffectionQuery);
+
+if (!$updatePetAffectionQueryResult) {
+	echo json_encode([
+		"status_code" => 23,
+		"error_message" => "Update Pet Affection Query Failed"
+	]);
+	exit();
+}
+
+echo json_encode([
+	"status_code" => $status_code,
+	"error_message" => $error_message
+]);
+?>
